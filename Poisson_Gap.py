974529db99c03ce3812a3113af266cb636b0c10c
emaciejewski <emaciejewski@v2-1-0vm2.bioscience-ct.net>
2017-01-08 22:30:28 -0500

    frequency array attempt

--- Poisson_Gap.py
+++ Poisson_Gap.py
@@ -6,8 +6,7 @@
 import argparse
 import random
 import math
-import string
-import sys
+import numpy as np
 
 
 
@@ -23,41 +22,59 @@ def poisson(lmbd):
 
     return k-1
 
+
+def frequency_array(np_array):
+    freq=np.bincount(np_array)
+    nums=np.arange(np.amax(np_array)+1)
+    print(np.vstack((nums,freq[nums])).T)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--seed", required=True, type=int, help="initial seed")
     parser.add_argument("-sp", "--sampling_points", required=True, type=int, help="number of sampling points")
     parser.add_argument("-tp", "--total_points", required=True, type=int, help="total number of points")
     args = parser.parse_args()
-    initial_seed = string.atof(args.seed)
-    sampling_points = string.atoi(args.sampling_points)
-    total_size = string.atoi(args.total_points)
-    ld = total_size/sampling_points
-    weight = 2.0
-    current_num_points = 0
-    random.seed(initial_seed)
-    
 
-    while current_num_points != sampling_points:
-        gap_size = 0
+    final = np.array([])
+    count = 0
+    total = 3
+    initial_seed = args.seed
+
+    while count != total:
+
+        
+        sampling_points = args.sampling_points
+        total_size = args.total_points
+        ld = total_size/sampling_points
+        weight = 2.0
         current_num_points = 0
-        vector_smpl_points = []
-
-        while gap_size < total_size:
-            vector_smpl_points.append(gap_size)
-            gap_size += 1
-            current_num_points += 1
-
-            gap_size += poisson((ld-1.0)*weight*math.sin(gap_size/(total_size+1)*math.pi))
-
-        if current_num_points > sampling_points:
-            weight *= 1.02
-       
-        if current_num_points < sampling_points:
-            weight /= 1.02
+        random.seed(initial_seed)
     
-    for num in range(len(vector_smpl_points)):
-        print vector_smpl_points[num]
+
+        while current_num_points != sampling_points:
+            gap_size = 0
+            current_num_points = 0
+            vector_smpl_points = np.array([])
+
+            while gap_size < total_size:
+                vector_smpl_points = np.append(vector_smpl_points, math.floor(gap_size))
+                gap_size += 1
+                current_num_points += 1
+
+                gap_size += int(poisson((ld-1.0)*weight*math.sin(gap_size/(total_size+1)*math.pi)))
+
+            if current_num_points > sampling_points:
+                weight *= 1.02
+       
+            if current_num_points < sampling_points:
+                weight /= 1.02
+    
+        initial_seed += 1
+        count += 1
+        final = np.append(final, vector_smpl_points)
+
+    frequency_array(final)
 
 main()
 
